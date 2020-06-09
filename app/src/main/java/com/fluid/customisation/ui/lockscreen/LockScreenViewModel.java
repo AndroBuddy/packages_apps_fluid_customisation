fe41d6d9e676f1e4d20e23e8fdfd48950e175648
AndroBuddy <anirudhchimpidi8@gmail.com>
2020-06-09 19:41:08 +0530

    Add dark mode, improve theming, add app icon

--- app/src/main/java/com/fluid/customisation/ui/lockscreen/LockScreenViewModel.java
+++ app/src/main/java/com/fluid/customisation/ui/lockscreen/LockScreenViewModel.java
@@ -1,8 +1,8 @@
 package com.fluid.customisation.ui.lockscreen;
 
-import android.arch.lifecycle.LiveData;
-import android.arch.lifecycle.MutableLiveData;
-import android.arch.lifecycle.ViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
 
 public class LockScreenViewModel extends ViewModel {
 
--- app/src/main/java/com/fluid/customisation/ui/statusbar/StatusBarViewModel.java
+++ app/src/main/java/com/fluid/customisation/ui/statusbar/StatusBarViewModel.java
@@ -1,8 +1,8 @@
 package com.fluid.customisation.ui.statusbar;
 
-import android.arch.lifecycle.LiveData;
-import android.arch.lifecycle.MutableLiveData;
-import android.arch.lifecycle.ViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
 
 public class StatusBarViewModel extends ViewModel {
 
